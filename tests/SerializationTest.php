a1b67dacb5a2a26cfa9842549cfd5871c0a65aee
Florian Reimair <f.reimair@conecto.at>
2022-03-02 09:07:35 +0100

    Serialize the demo request

--- tests/SerializationTest.php
+++ tests/SerializationTest.php
@@ -7,6 +7,10 @@ use Feratel\DSI\KeyValuesType;
 use Feratel\DSI\RangeType;
 use Feratel\DSI\RequestedWithTranslationType;
 use Feratel\DSI\RequestType;
+use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\BaseTypesHandler;
+use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\XmlSchemaDateHandler;
+use JMS\Serializer\Handler\HandlerRegistryInterface;
+use JMS\Serializer\SerializerBuilder;
 use PHPUnit\Framework\TestCase;
 
 final class SerializationTest extends TestCase
@@ -42,6 +46,34 @@ final class SerializationTest extends TestCase
         $dsiRequest = new FeratelDsiRQ();
         $dsiRequest->setRequest($request);
 
-        $this->assertTrue(true);
+        // serialize
+        // - setup serializer with custom types to deal with xsd2php's way of dates and lists.
+        $serializerBuilder = SerializerBuilder::create();
+        $serializerBuilder->addMetadataDir("../generated/metadata", "Feratel\DSI");
+        $serializerBuilder->configureHandlers(function (HandlerRegistryInterface $handler) use ($serializerBuilder) {
+            $serializerBuilder->addDefaultHandlers();
+            $handler->registerSubscribingHandler(new BaseTypesHandler()); // XMLSchema List handling
+            $handler->registerSubscribingHandler(new XmlSchemaDateHandler()); // XMLSchema date handling
+        });
+        $serializer = $serializerBuilder->build();
+
+        // - serialize
+        $result = $serializer->serialize($dsiRequest, 'xml');
+
+        $expected = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<FeratelDsiRQ xmlns="http://interface.deskline.net/DSI/XSD">
+  <Request Originator="FERATEL" Company="FERATEL">
+    <Range Code="RG">
+      <Item Id="F180FFD5-4FBF-4F2C-AC00-7E8B94462F2C"/>
+    </Range>
+    <KeyValues GetLocalValues="false" DateFrom="2000-01-01">
+      <Towns Show="true" IncludeTranslations="false"/>
+    </KeyValues>
+  </Request>
+</FeratelDsiRQ>
+
+XML;
+        $this->assertEquals($expected, $result);
     }
 }
