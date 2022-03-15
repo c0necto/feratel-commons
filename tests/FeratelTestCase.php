2a6baccc9406e36e686baf69331c88a4d6d8421b
Florian Reimair <f.reimair@conecto.at>
2022-03-15 16:04:01 +0100

    Move generated files to src/

---
+++ tests/FeratelTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\BaseTypesHandler;
+use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\XmlSchemaDateHandler;
+use JMS\Serializer\Handler\HandlerRegistryInterface;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+use PHPUnit\Framework\TestCase;
+
+class FeratelTestCase extends TestCase
+{
+    protected mixed $config;
+    protected Serializer $serializer;
+
+    public function setUp(): void
+    {
+        // - setup serializer with custom types to deal with xsd2php's way of dates and lists.
+        $serializerBuilder = SerializerBuilder::create();
+        $serializerBuilder->addMetadataDir("./src/generated/DSI/metadata", "Feratel\DSI");
+        $serializerBuilder->configureHandlers(function (HandlerRegistryInterface $handler) use ($serializerBuilder) {
+            $serializerBuilder->addDefaultHandlers();
+            $handler->registerSubscribingHandler(new BaseTypesHandler()); // XMLSchema List handling
+            $handler->registerSubscribingHandler(new XmlSchemaDateHandler()); // XMLSchema date handling
+        });
+        $this->serializer = $serializerBuilder->build();
+
+        $this->config = include('./feratel.conf.php');
+    }
+}
--- tests/SerializationTest.php
+++ tests/SerializationTest.php
@@ -1,5 +1,5 @@
 <?php
-require('../vendor/autoload.php');
+require('./vendor/autoload.php');
 
 use Feratel\DSI\FeratelDsiRQ;
 use Feratel\DSI\FeratelDsiRS;
@@ -8,30 +8,9 @@ use Feratel\DSI\KeyValuesType;
 use Feratel\DSI\RangeType;
 use Feratel\DSI\RequestedWithTranslationType;
 use Feratel\DSI\RequestType;
-use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\BaseTypesHandler;
-use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\XmlSchemaDateHandler;
-use JMS\Serializer\Handler\HandlerRegistryInterface;
-use JMS\Serializer\Serializer;
-use JMS\Serializer\SerializerBuilder;
-use PHPUnit\Framework\TestCase;
 
-final class SerializationTest extends TestCase
+final class SerializationTest extends FeratelTestCase
 {
-    private Serializer $serializer;
-
-    protected function setUp(): void
-    {
-        // - setup serializer with custom types to deal with xsd2php's way of dates and lists.
-        $serializerBuilder = SerializerBuilder::create();
-        $serializerBuilder->addMetadataDir("../generated/metadata", "Feratel\DSI");
-        $serializerBuilder->configureHandlers(function (HandlerRegistryInterface $handler) use ($serializerBuilder) {
-            $serializerBuilder->addDefaultHandlers();
-            $handler->registerSubscribingHandler(new BaseTypesHandler()); // XMLSchema List handling
-            $handler->registerSubscribingHandler(new XmlSchemaDateHandler()); // XMLSchema date handling
-        });
-        $this->serializer = $serializerBuilder->build();
-    }
-
     public function testSerialization(): void
     {
         // authenticate the request
