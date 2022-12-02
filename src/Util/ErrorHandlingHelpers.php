668daad7370f98fb183a5e02351af9ebe819a73f
Florian Reimair <f.reimair@conecto.at>
2022-12-02 09:20:39 +0100

    Import ErrorHandlingHelper

---
+++ src/Util/ErrorHandlingHelpers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helpers;
+
+use Error;
+use Exception;
+
+class ErrorHandlingHelpers
+{
+
+    /**
+     * It is not pretty, however, it follows the example of PHP RFC: Catchable "call to a member function of a non-object"
+     * {@link https://wiki.php.net/rfc/catchable-call-to-member-of-non-object}
+     *
+     * @param Exception|Error $exception
+     * @return false|int
+     */
+    public static function isMethodCallOnNullObjectException(Exception|Error $exception): int|false
+    {
+        return preg_match("/Call to a member function .* on null/", $exception->getMessage());
+    }
+}
